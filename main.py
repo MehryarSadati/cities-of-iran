e2de47ca201b245d77121e902914af4b4bdd1359
Mehryar Sadati <mehryarsadati@gmail.com>
2024-08-31 20:40:21 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import polars as pl
+
+cities = pl.read_csv("csv/shahr.csv")
+provinces = pl.read_csv("csv/ostan.csv")
+
+city = input()
+result = cities.filter(pl.col("name") == city)
+province_code = result["ostan"].item() if result.height > 0 else None
+
+# print(type(province_code))
+
+province_name = provinces.filter(pl.col("id") == province_code)
+final_answer = province_name["name"].to_list
+
+if province_code:
+    print(final_answer)
+
+else:
+    print(f"sorry we don't have the city of {city} in our dataset")
